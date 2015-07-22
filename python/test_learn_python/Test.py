19e7e5758eed6ef4c1f831f1b99449c705783ce4
Ivon Liu <ivon.liu2011@gmail.com>
2015-07-22 07:20:40 -0700

    More Python testing

--- python/test_learn_python/Test.py
+++ python/test_learn_python/Test.py
@@ -1,4 +1,17 @@
 __author__ = 'MES'
 
 print("Hello world")
-anArray = [3, 1, 4, 1, 5]
+anArray = [[1, 2, 3, 4, 5],
+           [6, 7, 8, 9, 10],
+           [11, 12, 13, 14, 15]]
+
+for x in anArray:
+    print(x)
+    print("hello")
+print("out of loop")
+
+def testFunction(array, row):
+    for x in array[row]:
+        print(x)
+    
+testFunction(anArray, 2)
