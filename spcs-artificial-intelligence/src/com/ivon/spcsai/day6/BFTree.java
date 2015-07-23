149cd0a0652abea66782386a580a0bfd01b6664e
Ivon Liu <ivon.liu2011@gmail.com>
2015-07-23 15:32:19 -0700

    Try to make a decision tree

---
+++ spcs-artificial-intelligence/src/com/ivon/spcsai/day6/BFTree.java
@@ -0,0 +1,28 @@
+package com.ivon.spcsai.day6;
+
+/**
+ * Created by MES on 7/22/2015.
+ */
+public class BFTree {
+
+    public static void main(String[] args) {
+        System.out.println(entropy(0.5, 0.5));
+    }
+
+    private static double informationGain() {
+        entropy(5, 6, 3, 766, 3, 6.2342342, 3.1415, 0);
+        return 0;
+    }
+
+    private static double entropy(double... probabilities) {
+        double entropy = 0;
+        for (double p : probabilities) {
+            entropy += (-1 * p * (Math.log(p)/Math.log(2)));
+        }
+        return entropy;
+    }
+
+    private static double conditionalEntropy() {
+        return 0;
+    }
+}
