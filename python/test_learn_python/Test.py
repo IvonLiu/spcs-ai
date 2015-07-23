293e9716d4847f621dfd9cc42ef416b243c9af07
Ivon Liu <ivon.liu2011@gmail.com>
2015-07-23 14:43:04 -0700

    Commit test python stuff

--- python/test_learn_python/Test.py
+++ python/test_learn_python/Test.py
@@ -5,13 +5,15 @@ anArray = [[1, 2, 3, 4, 5],
            [6, 7, 8, 9, 10],
            [11, 12, 13, 14, 15]]
 
-for x in anArray:
-    print(x)
+for x in range(0, 3, 2):
+    print(anArray[x])
     print("hello")
 print("out of loop")
 
-def testFunction(array, row):
+
+def test_function(array, row):
     for x in array[row]:
         print(x)
-    
-testFunction(anArray, 2)
+
+
+test_function(anArray, 2)
